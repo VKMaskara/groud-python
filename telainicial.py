4c92831c7a7ec30ff9631c7a277384f2c05508bc
SouresSam <samara.soares1032@gmail.com>
2025-11-26 20:23:40 -0300

    tela inicial

--- telainicial.py
+++ telainicial.py
@@ -1,7 +1,14 @@
 import design
 import time
+design.limpar_tela()
+
 #Tela inicial - Samara Soares
+print("Bem vindo...") #Mensagem inicial que irá aparecer para o usuario
 
-print("Bem vindo...")
 
 
+#print("Toque qualquer tecla para continuar") 
+
+
+
+design.loading()
