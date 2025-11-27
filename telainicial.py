0d1f4073f39339955074882a6dcca17b8968188c
SouresSam <samara.soares1032@gmail.com>
2025-11-26 21:57:35 -0300

    .

--- telainicial.py
+++ telainicial.py
@@ -3,13 +3,9 @@ import time
 design.limpar_tela()
 
 #Tela inicial - Samara Soares
-print("Bem vindo...") #Mensagem inicial que irá aparecer para o usuario
+print("Bem vindo ao site Macados da Programação!") #Mensagem inicial que irá aparecer para o usuario
 
 
 
 #print("Toque qualquer tecla para continuar") 
-
-
-
-design.loading()
 
