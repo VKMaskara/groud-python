e19cc798586cf626faed2ed05969c5377c57d9e8
SouresSam <samara.soares1032@gmail.com>
2025-12-02 20:42:12 -0300

    Create tela_inicial.py

---
+++ tela_inicial.py
@@ -0,0 +1,14 @@
+import design #importa o arquivo design.py
+import time #importa a biblioteca time para usar a função sleep
+design.limpar_tela()
+
+#tela inicial - Samara Soares
+
+design.titulo_secao("Seja muito bem vindo ao programa dos Macacos da Programação!") #mensagem inicial que irá aparecer para o usuario
+
+design.container("Toque qualquer tecla para continuar") #texto instruindo o usuario a continuar
+(input()) #usuario digita qualquer tecla para continuar
+
+design.limpar_tela() #função que limpa a tela
+
+#aqui vai a função que vai levar o usuario para a tela login 
